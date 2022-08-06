d5519b75e92a3b4badcf4df5e1898344c7541f20
JackKyle <95871879+JackKyle@users.noreply.github.com>
2022-08-06 21:47:33 +0100

    Add files via upload

--- objectCreation.js
+++ objectCreation.js
@@ -1,23 +1,27 @@
-import { mainLoop, frictionZone, Ball } from './physics.js';
+import { mainLoop, FrictionZone, Ball, Magnet } from './physics.js';
 
-let Friction1 = new frictionZone(500, 350, 500, 200, "black", "purple", 1);
+let Friction1 = new FrictionZone(500, 350, 500, 200, "black", "purple", 0.05);
 
-let Friction2 = new frictionZone(1000, 550, 100, 100, "black", "purple", 1);
+let Friction2 = new FrictionZone(1000, 550, 100, 100, "black", "purple", 0.05);
+
+let magnet1 = new Magnet (1000, 700, 20, 1, "black", "white");
+
+let magnet2 = new Magnet (1000, 850, 20, -1, "black", "white");
 
 let Ball1 = new Ball(100, 100, 20, 250, "black", "red");
-Ball1.velocity.x_vector = 10;
-Ball1.velocity.y_vector = 5;
+Ball1.velocity.x_vector = 5;
+Ball1.velocity.y_vector = 2.5;
 
 let Ball2 = new Ball(1300, 700, 15, 100, "black", "yellow");
-Ball2.velocity.x_vector = -10;
-Ball2.velocity.y_vector = -5;
+Ball2.velocity.x_vector = -5;
+Ball2.velocity.y_vector = -2.5;
 
-let Ball3 = new Ball(100, 700, 30, 1, "black", "pink");
+let Ball3 = new Ball(100, 700, 30, 50, "black", "pink");
 Ball3.velocity.x_vector = 2;
 Ball3.velocity.y_vector = -1;
 
-let Ball4 = new Ball(1300, 100, 20, 1, "black", "black");
-Ball4.velocity.x_vector =-10;
-Ball4.velocity.y_vector = 5;
+let Ball4 = new Ball(1300, 100, 20, 50, "black", "black");
+Ball4.velocity.x_vector =-5;
+Ball4.velocity.y_vector = 2.5;
 
 requestAnimationFrame(mainLoop);
--- physics.js
+++ physics.js
@@ -1,6 +1,8 @@
 import { ctx, width, height } from './engineConfig.js';
 const BallArray = [];
 const frictionSquareArray = [];
+const magnetArray = [];
+let friction = 0.000001;
 class Vector{
     constructor(x_vector, y_vector){
         this.x_vector = x_vector;
@@ -43,7 +45,7 @@ class Ball{
     }
 
     //displaying the current acceleration and the velocity of the ball
-    display(){
+    speedDisplay(){
         ctx.beginPath();
         ctx.moveTo(this.centre.x_vector, this.centre.y_vector);
         ctx.lineTo(this.centre.x_vector + this.acceleration.x_vector*100, this.centre.y_vector + this.acceleration.y_vector*100);
@@ -59,7 +61,7 @@ class Ball{
     }
 }
 
-class frictionZone{
+class FrictionZone{
     constructor(upperxpoint, upperypoint, xlength, ylength, outline, fill, friction){
         this.upperpoint = new Vector (upperxpoint, upperypoint);
         this.length = new Vector (xlength, ylength);
@@ -80,27 +82,104 @@ class frictionZone{
     }
 }
 
-let friction = 0.001;
-function BallVectors1(ball_1, square_1){
+class Magnet{
+    constructor(xpoint, ypoint, radius, magnetism, outline, fill){
+        this.centre = new Vector (xpoint, ypoint);
+        this.radius = radius;
+        this.magnetism = magnetism;
+        this.outline = outline;
+        this.fill = fill;
+        magnetArray.push(this);
+    }
+    drawMagnet(){
+        ctx.beginPath();
+        ctx.arc(this.centre.x_vector, this.centre.y_vector, this.radius, 0, 2*Math.PI);
+        ctx.strokeStyle = this.outline;
+        ctx.stroke();
+        ctx.fillStyle = this.fill;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+function BallVectors(ball_1, ball_2){
+    //distance between two centres in x and y directions
+    let distance_x = ball_1.centre.x_vector-ball_2.centre.x_vector;
+    let distance_y = ball_1.centre.y_vector-ball_2.centre.y_vector;
+    //magnitude of distance to give a scalar number distance between two centres
+    let distance_magnitude = Math.sqrt(distance_x**2+distance_y**2);
+    if (ball_1.radius+ball_2.radius>=distance_magnitude){
+        if (ball_1.radius+ball_2.radius>distance_magnitude){
+            //overlap between balls (if 0 then no overlap)
+            let diff = ball_1.radius+ball_2.radius - distance_magnitude;
+            //calculating amount of distance each ball must be moved to make them no longer overlap but still touch
+            //calculated by taking the total distance in each plane and dividing it by the total distance magnitude and multiplying 
+            //it by the overlap divided by two so that it takes the fact they are each moving half the distance into account
+            //each is then added to ball 1 and subtracted from ball 2 in order to ensure each ball moves an equal amount 
+            //from the other, producing no overlap in the end
+            let move_x = (distance_x/distance_magnitude)*(diff/2);
+            let move_y = (distance_y/distance_magnitude)*(diff/2);
+            ball_1.centre.x_vector += move_x;
+            ball_1.centre.y_vector += move_y;
+            ball_2.centre.x_vector -= move_x;
+            ball_2.centre.y_vector -= move_y;
+        }
+        //normal vector of collision found from taking centre of circle 1 from centre of circle 2
+        let normalVector = new Vector(ball_2.centre.x_vector - ball_1.centre.x_vector, 
+        ball_2.centre.y_vector - ball_1.centre.y_vector);
+        //unit vector of normal found from dividing both components of vector by magnitude
+        let unitNormalVector = new Vector(normalVector.x_vector/normalVector.magnitude(), 
+        normalVector.y_vector/normalVector.magnitude());
+        //get unit tangent vector of collision 
+        let unitTangentVector = new Vector(unitNormalVector.y_vector*-1, unitNormalVector.x_vector);
+        //get initial ball 1 normal direction vector by taking dot product of ball's velocity and that of the unit normal vector
+        let initialBall1Normal = ball_1.velocity.dotproduct(unitNormalVector);
+        //get initial ball 1 tangent direction vector by taking dot product of ball's velocity and that of unit tangent vector
+        let ball1Tangent = ball_1.velocity.dotproduct(unitTangentVector);
+        let initialBall2Normal = ball_2.velocity.dotproduct(unitNormalVector);
+        let ball2Tangent = ball_2.velocity.dotproduct(unitTangentVector);
+        //final ball 1 normal velocity taken from formula 
+        let finalBall1Normal = ((initialBall1Normal*(ball_1.mass-ball_2.mass))+(2*ball_2.mass*initialBall2Normal))/
+        (ball_1.mass+ball_2.mass);
+        let finalBall2Normal = ((initialBall2Normal*(ball_2.mass-ball_1.mass))+(2*ball_1.mass*initialBall1Normal))/
+        (ball_1.mass+ball_2.mass);
+        //calculate new ball normal velocity vectors by multiplying each vector of unit normal vector by 
+        //the final ball normal velocity
+        let newBall1NormalVectorVelocity = new Vector (unitNormalVector.x_vector*finalBall1Normal, 
+        unitNormalVector.y_vector*finalBall1Normal);
+        let newBall2NormalVectorVelocity= new Vector (unitNormalVector.x_vector*finalBall2Normal, 
+        unitNormalVector.y_vector*finalBall2Normal);
+        //calculate new ball tangent velocity vectors by multiplying each vector of unit tangent vector by 
+        //the final ball tangent velocity
+        let newBall1TangentVectorVelocity = new Vector (unitTangentVector.x_vector*ball1Tangent, 
+        unitTangentVector.y_vector*ball1Tangent);
+        let newBall2TangentVectorVelocity = new Vector (unitTangentVector.x_vector*ball2Tangent, 
+        unitTangentVector.y_vector*ball2Tangent);
+        //calculate each ball's velocities by adding that of the normal vector and tangent vector velocity
+        ball_1.velocity.x_vector = newBall1NormalVectorVelocity.x_vector+newBall1TangentVectorVelocity.x_vector;
+        ball_1.velocity.y_vector = newBall1NormalVectorVelocity.y_vector+newBall1TangentVectorVelocity.y_vector;
+        ball_2.velocity.x_vector = newBall2NormalVectorVelocity.x_vector+newBall2TangentVectorVelocity.x_vector;
+        ball_2.velocity.y_vector = newBall2NormalVectorVelocity.y_vector+newBall2TangentVectorVelocity.y_vector;
+    };
+}
+
+//function to add acceleration to the velocity
+function addAcceleration(ball_1){
     ball_1.velocity.x_vector += ball_1.acceleration.x_vector;
     ball_1.velocity.y_vector += ball_1.acceleration.y_vector;
+}
 
-    if (ball_1.centre.y_vector <= ball_1.radius || ball_1.centre.y_vector >= (height-ball_1.radius)){
-        if (ball_1.centre.y_vector<=ball_1.radius){
-            ball_1.centre.y_vector = ball_1.radius;
-        } else if (ball_1.centre.y_vector >= (height-ball_1.radius)){
-            ball_1.centre.y_vector = height - ball_1.radius;
-        }
-        ball_1.velocity.y_vector = ball_1.velocity.y_vector * -1;
-    }
-    if (ball_1.centre.x_vector <= ball_1.radius || ball_1.centre.x_vector >= (width-ball_1.radius)){
-        if (ball_1.centre.x_vector<=ball_1.radius){
-            ball_1.centre.x_vector = ball_1.radius;
-        } else if (ball_1.centre.x_vector >= (width-ball_1.radius)){
-            ball_1.centre.x_vector = width - ball_1.radius;
-        }
-        ball_1.velocity.x_vector = -1 * ball_1.velocity.x_vector;
-    } 
+//function to add velocity to the placement of the ball
+function addVelocity(ball_1){
+    ball_1.centre.x_vector += ball_1.velocity.x_vector;
+    ball_1.centre.y_vector += ball_1.velocity.y_vector;
+}
+
+//function to check for friction zones
+function checkFriction(ball_1, square_1){
+    //checks that the centre of the ball (the point that would be touching the surface of the friction zone if ball
+    //is considered a 2d representation of a sphere) is within the coordinates of the zone and applies friction of
+    //zone to the ball
     if ((ball_1.centre.x_vector>=square_1.upperpoint.x_vector&&ball_1.centre.x_vector<=
         (square_1.upperpoint.x_vector+square_1.length.x_vector)) && 
         (ball_1.centre.y_vector>=square_1.upperpoint.y_vector&&ball_1.centre.y_vector<=
@@ -111,22 +190,13 @@ function BallVectors1(ball_1, square_1){
         ball_1.velocity.x_vector *= 1 - friction;
         ball_1.velocity.y_vector *= 1 - friction;
     }
-    
-    ball_1.centre.x_vector += ball_1.velocity.x_vector;
-    ball_1.centre.y_vector += ball_1.velocity.y_vector;
 }
-function BallVectors2(ball_1, ball_2, square_1){
-    //acceleration values added to the velocity components
-    ball_1.velocity.x_vector += ball_1.acceleration.x_vector;
-    ball_1.velocity.y_vector += ball_1.acceleration.y_vector;
-        if ((ball_1.centre.x_vector>=square_1.upperpoint.x_vector&&ball_1.centre.x_vector<=
-            (square_1.upperpoint.x_vector+square_1.length.x_vector)) && 
-            (ball_1.centre.y_vector>=square_1.upperpoint.y_vector&&ball_1.centre.y_vector<=
-            (square_1.upperpoint.y_vector+square_1.length.y_vector))){
-                ball_1.velocity.x_vector *= 1 - square_1.friction;
-                ball_1.velocity.y_vector *= 1 - square_1.friction;
-        };
-    
+
+//function to check if the ball is against the edges 
+function checkEdges(ball_1){
+    //checks that ball isn't past border of screen and reverses vector dependent on side it hits
+    //also brings ball back to edge if it were to cross the border due to the small jumps the ball
+    //actually makes to simulate movement
     if (ball_1.centre.x_vector <= ball_1.radius || ball_1.centre.x_vector >= (width-ball_1.radius)){
         if (ball_1.centre.x_vector<=ball_1.radius){
             ball_1.centre.x_vector = ball_1.radius;
@@ -135,6 +205,7 @@ function BallVectors2(ball_1, ball_2, square_1){
         }
         ball_1.velocity.x_vector = -1 * ball_1.velocity.x_vector;
     }
+
     if (ball_1.centre.y_vector <= ball_1.radius || ball_1.centre.y_vector >= (height-ball_1.radius)){
         if (ball_1.centre.y_vector<=ball_1.radius){
             ball_1.centre.y_vector = ball_1.radius;
@@ -143,104 +214,57 @@ function BallVectors2(ball_1, ball_2, square_1){
         }
         ball_1.velocity.y_vector = ball_1.velocity.y_vector * -1;
     }
-    ball_1.momentum = ball_1.mass*ball_1.velocity.magnitude();
-    //velocity gets multiplied by a number between 0 and 1
-    //ball_1.vel_x *= 1-friction;
-    //ball_1.vel_y *= 1-friction;
-            //distance between two centres in x and y directions
-            let distance_x = ball_1.centre.x_vector-ball_2.centre.x_vector;
-            let distance_y = ball_1.centre.y_vector-ball_2.centre.y_vector;
-            //magnitude of distance to give a scalar number distance between two centres
-            let distance_magnitude = Math.sqrt(distance_x**2+distance_y**2);
-            if (ball_1.radius+ball_2.radius>=distance_magnitude){
-                if (ball_1.radius+ball_2.radius>distance_magnitude){
-                    //overlap between balls (if 0 then no overlap)
-                    let diff = ball_1.radius+ball_2.radius - distance_magnitude;
-                    //calculating amount of distance each ball must be moved to make them no longer overlap but still touch
-                    //calculated by taking the total distance in each plane and dividing it by the total distance magnitude and multiplying 
-                    //it by the overlap divided by two so that it takes the fact they are each moving half the distance into account
-                    //each is then added to ball 1 and subtracted from ball 2 in order to ensure each ball moves an equal amount 
-                    //from the other, producing no overlap in the end
-                    let move_x = (distance_x/distance_magnitude)*(diff/2);
-                    let move_y = (distance_y/distance_magnitude)*(diff/2);
-                    ball_1.centre.x_vector += move_x;
-                    ball_1.centre.y_vector += move_y;
-                    ball_2.centre.x_vector -= move_x;
-                    ball_2.centre.y_vector -= move_y;
-                }
-                //normal vector of collision found from taking centre of circle 1 from centre of circle 2
-                let normalVector = new Vector(ball_2.centre.x_vector - ball_1.centre.x_vector, 
-                ball_2.centre.y_vector - ball_1.centre.y_vector);
-                //unit vector of normal found from dividing both components of vector by magnitude
-                let unitNormalVector = new Vector(normalVector.x_vector/normalVector.magnitude(), 
-                normalVector.y_vector/normalVector.magnitude());
-                //get unit tangent vector of collision 
-                let unitTangentVector = new Vector(unitNormalVector.y_vector*-1, unitNormalVector.x_vector);
-                //get initial ball 1 normal direction vector by taking dot product of ball's velocity and that of the unit normal vector
-                let initialBall1Normal = ball_1.velocity.dotproduct(unitNormalVector);
-                //get initial ball 1 tangent direction vector by taking dot product of ball's velocity and that of unit tangent vector
-                let ball1Tangent = ball_1.velocity.dotproduct(unitTangentVector);
-                let initialBall2Normal = ball_2.velocity.dotproduct(unitNormalVector);
-                let ball2Tangent = ball_2.velocity.dotproduct(unitTangentVector);
-                //final ball 1 normal velocity taken from formula 
-                let finalBall1Normal = ((initialBall1Normal*(ball_1.mass-ball_2.mass))+(2*ball_2.mass*initialBall2Normal))/
-                (ball_1.mass+ball_2.mass);
-                let finalBall2Normal = ((initialBall2Normal*(ball_2.mass-ball_1.mass))+(2*ball_1.mass*initialBall1Normal))/
-                (ball_1.mass+ball_2.mass);
-                //calculate new ball normal velocity vectors by multiplying each vector of unit normal vector by 
-                //the final ball normal velocity
-                let newBall1NormalVectorVelocity = new Vector (unitNormalVector.x_vector*finalBall1Normal, 
-                unitNormalVector.y_vector*finalBall1Normal);
-                let newBall2NormalVectorVelocity= new Vector (unitNormalVector.x_vector*finalBall2Normal, 
-                unitNormalVector.y_vector*finalBall2Normal);
-                //calculate new ball tangent velocity vectors by multiplying each vector of unit tangent vector by 
-                //the final ball tangent velocity
-                let newBall1TangentVectorVelocity = new Vector (unitTangentVector.x_vector*ball1Tangent, 
-                unitTangentVector.y_vector*ball1Tangent);
-                let newBall2TangentVectorVelocity = new Vector (unitTangentVector.x_vector*ball2Tangent, 
-                unitTangentVector.y_vector*ball2Tangent);
-                //calculate each ball's velocities by adding that of the normal vector and tangent vector velocity
-                ball_1.velocity.x_vector = newBall1NormalVectorVelocity.x_vector+newBall1TangentVectorVelocity.x_vector;
-                ball_1.velocity.y_vector = newBall1NormalVectorVelocity.y_vector+newBall1TangentVectorVelocity.y_vector;
-                ball_2.velocity.x_vector = newBall2NormalVectorVelocity.x_vector+newBall2TangentVectorVelocity.x_vector;
-                ball_2.velocity.y_vector = newBall2NormalVectorVelocity.y_vector+newBall2TangentVectorVelocity.y_vector;
-            };
-            if ((ball_1.centre.x_vector>=square_1.upperpoint.x_vector&&ball_1.centre.x_vector<=
-                (square_1.upperpoint.x_vector+square_1.length.x_vector)) && 
-                (ball_1.centre.y_vector>=square_1.upperpoint.y_vector&&ball_1.centre.y_vector<=
-                (square_1.upperpoint.y_vector+square_1.length.y_vector))){
-                    ball_1.velocity.x_vector *= 1 - square_1.friction;
-                    ball_1.velocity.y_vector *= 1 - square_1.friction;
-            } else {
-                ball_1.velocity.x_vector *= 1 - friction;
-                ball_1.velocity.y_vector *= 1 - friction;
-            }
-            //velocity values added to the current x, y position
-    ball_1.centre.x_vector += ball_1.velocity.x_vector;
-    ball_1.centre.y_vector += ball_1.velocity.y_vector;
-    
 }
 
+//function to check magnetism against balls currently moving
+function checkMagnets(ball_1, magnet_1){
+    let distance_x = magnet_1.centre.x_vector-ball_1.centre.x_vector;
+    let distance_y = magnet_1.centre.y_vector-ball_1.centre.y_vector;
+    //magnitude of distance to give a scalar number distance between two centres
+    let distance_magnitude = Math.sqrt(distance_x**2+distance_y**2);
+    let magnetInverseSquare = 1/(distance_magnitude**2);
+    let velocity_x = distance_x*magnetInverseSquare*magnet_1.magnetism;
+    let velocity_y = distance_y*magnetInverseSquare*magnet_1.magnetism;
+    ball_1.velocity.x_vector += velocity_x;
+    ball_1.velocity.y_vector += velocity_y;
+}
 
-
+//group of functions to avoid code repetition
+function functionGroup(ball_1){
+    checkEdges(ball_1);
+    frictionSquareArray.forEach(square_1 => {
+        checkFriction(ball_1, square_1); 
+    });
+    magnetArray.forEach(magnet_1 => {
+        checkMagnets(ball_1, magnet_1);
+    });
+    addAcceleration(ball_1);
+    addVelocity(ball_1);
+}
 function mainLoop() {
+
     ctx.clearRect(0, 0, width, height);
+    if (frictionSquareArray.length === 0){
+        let Friction1 = new FrictionZone(0, 0, 0, 0, "green", "green", 0);
+    }    
     frictionSquareArray.forEach((square_1) => {
         square_1.drawSquare();
+    });
+    magnetArray.forEach((magnet_1) => {
+        magnet_1.drawMagnet();
+    });
     BallArray.forEach((ball_1, index) => {
         ball_1.drawBall();
         if (index !== BallArray.length-1){
             for(let num = index+1; num<BallArray.length; num++){
-                BallVectors2(BallArray[index], BallArray[num], square_1);
+                BallVectors(ball_1, BallArray[num]);
             }
+            functionGroup(ball_1);
         } else {
-            BallVectors1(ball_1, square_1);
+            functionGroup(ball_1);
         }
-        
-        
-        ball_1.display();
+        ball_1.speedDisplay();
     });
-});
     requestAnimationFrame(mainLoop);
 }
-export { mainLoop, frictionZone, Ball};
+export { mainLoop, FrictionZone, Ball, Magnet};
